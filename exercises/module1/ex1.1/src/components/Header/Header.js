9ee3f50041a6fba5751132ea5c18882148298521
François Bardijn <francois.bardijn@hotmail.com>
2022-09-20 14:35:34 +0200

    1.1-1.2 : import image

--- exercises/module1/ex1.1/src/components/Header/Header.js
+++ exercises/module1/ex1.1/src/components/Header/Header.js
@@ -1,7 +1,10 @@
+import imageRigolote from "./little_pony.jpg"
+
 const Header = (props) => {
     return (
       <>
         <h1>{props.course}</h1>
+        <img src={imageRigolote} alt="rigolote"></img>
       </>
     )
   }
