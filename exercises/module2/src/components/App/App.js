4278bfc5f1b45a34a590688c0ac5a94ca83e0422
François Bardijn <francois.bardijn@hotmail.com>
2022-09-30 11:01:42 +0200

    module3 : basics

--- exercises/module2/src/components/App/App.js
+++ exercises/module2/src/components/App/App.js
@@ -6,7 +6,15 @@ import useLocalStorage from "hooks/useLocalStorage"
 const App = () => {
   const [ counter, setCounter ] = useLocalStorage("counter", 0)
 
-  const changeCount = (delta) => {
+  // const changeCount = (delta) => {
+  //   localStorage.setItem("counter", JSON.stringify(counter + parseInt(delta)))
+  //   setCounter(counter + parseInt(delta))
+  // }
+
+  function handleClick(e) {
+    console.log("oui");
+    const delta = e.target.dataset.delta
+    console.log(e.target.attributes);
     localStorage.setItem("counter", JSON.stringify(counter + parseInt(delta)))
     setCounter(counter + parseInt(delta))
   }
@@ -14,9 +22,9 @@ const App = () => {
   return (
     <div>
       <Display counter={counter}/>
-      <Button onClick={changeCount} delta={1} text='plus'/>      
-      <Button onClick={changeCount} delta={-counter} text='zero'/>           
-      <Button onClick={changeCount} delta={-1} text='minus'/>               
+      <Button onClick={handleClick} data-delta="1" text='plus'/>      
+      <Button onClick={handleClick} data-delta={-counter} text='zero'/>           
+      <Button onClick={handleClick} data-delta="-1" text='minus'/>               
     </div>
   )
 }
